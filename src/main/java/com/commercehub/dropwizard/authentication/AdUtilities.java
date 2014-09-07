024d2305ff2b3807f1f4092946d94fdb4c6d279c
Gavin Hogan <gavin@commercehub.com>
2014-09-06 23:41:22 -0400

    #3 Ensure that the memberof attribute is always treated as a Set

--- src/main/java/com/commercehub/dropwizard/authentication/AdUtilities.java
+++ src/main/java/com/commercehub/dropwizard/authentication/AdUtilities.java
@@ -1,44 +1,56 @@
 package com.commercehub.dropwizard.authentication;
 
-import org.apache.commons.lang.text.StrSubstitutor;
+import com.commercehub.dropwizard.authentication.support.CaseInsensitiveHashMap;
 
 import javax.naming.NamingEnumeration;
 import javax.naming.NamingException;
 import javax.naming.directory.Attribute;
 import javax.naming.directory.Attributes;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 public class AdUtilities {
 
-    public static Map<String, Object> simplify(Attributes attributes){
-        Map<String, Object> result = new com.commercehub.dropwizard.authentication.support.CaseInsensitiveHashMap<>();
+    /**
+     * LDAP attributes can be challenging to deal with. The Java api only makes it more cumbersome.
+     * To simply things we are taking the following simplifying assumptions.
+     * 1. memberOf is always a multivalued Set
+     * 2. mail is always a single value, additional values are discarded
+     * 3. Other attributes that return more than one result will be treated as multivalued and will be added
+     *    as a set to the returned Map
+     * 4. Other attributes that return one value will be returned as Object they came back from the server as.
+     *
+     * Use a {@link com.commercehub.dropwizard.authentication.AdPrincipalMapper} if you want to process the resulting
+     * attributes with more wisdom.
+     *
+     * @param attributes The raw attributes returned from the ActiveDirectory domain controller
+     * @return a CaseInsensitiveHashMap with the simplified attribute model.
+     */
+    public static CaseInsensitiveHashMap<Object> simplify(Attributes attributes){
+        CaseInsensitiveHashMap<Object> result = new CaseInsensitiveHashMap<>();
         try {
             NamingEnumeration<? extends Attribute> attrs = attributes.getAll();
             while(attrs.hasMore()){
                 Attribute a = attrs.next();
                 Object val;
-                if(a.size()==1){
-                    val = a.get();
-                }else{
+                if(a.getID().toLowerCase().equals("mail")){
+                    val = a.get(0);
+                } else if (a.size()>1 || a.getID().toLowerCase().equals("memberof")){
                     Set valSet = new HashSet();
                     for(int i =0; i< a.size(); i++){
                         valSet.add(a.get(i));
                     }
                     val = valSet;
+                }else{
+                    val = a.get();
                 }
                 result.put(a.getID(), val);
             }
 
         } catch (NamingException e) {
-            //TODO
             e.printStackTrace();
         }
         return result;
     }
 
-    public static String expandTemplate(String template, Map<String, String> values) {
-        return StrSubstitutor.replace(template, values, "${", "}");
-    }
 }
--- src/main/java/com/commercehub/dropwizard/authentication/support/CaseInsensitiveHashMap.java
+++ src/main/java/com/commercehub/dropwizard/authentication/support/CaseInsensitiveHashMap.java
@@ -1,11 +1,23 @@
 package com.commercehub.dropwizard.authentication.support;
 
 import java.util.HashMap;
+import java.util.Map;
 
 /**
- * Created by ghogan on 8/16/14.
+ * Simple extension to {@link java.util.HashMap} which does not enforce case sensitivity
  */
 public class CaseInsensitiveHashMap<V> extends HashMap<String, V> {
+
+    public CaseInsensitiveHashMap(){
+        super();
+    }
+
+    public CaseInsensitiveHashMap(Map<String, V> m) {
+        super();
+        for(Entry<String, V> entry: m.entrySet()){
+            put(entry.getKey(), entry.getValue());
+        }
+    }
 
     @Override
     public V put(String key, V value) {
